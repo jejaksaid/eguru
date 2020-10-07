ae73f8ac3a2a56bb36b97004cfbb96685c44fd0a
jejaksaid <mail@saidramadhan.com>
2020-10-07 09:44:13 -0400

    710

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'eguru';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '4k4m3g4k1LLll';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'http://localhost/eguru';
+$CFG->dataroot  = '/opt/lampstack-7.4.10-0/apache2/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
